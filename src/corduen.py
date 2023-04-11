85423eb6f60884b370332bcea2eab7d3c2a8e22d
Handsomewl <wl.tsinghua.ee@gmail.com>
2023-04-11 19:37:07 +0800

    simplify

--- src/corduen.py
+++ src/corduen.py
@@ -1,13 +1,15 @@
 import numpy as np
-from src.baselines import greedyCharikar
 from scipy.sparse import lil_matrix,csc_matrix
+
+from src.baselines import greedyCharikar
 from src.boosting import fastNeighborBoosting
-from src.nncf import CNMF,CONMF
+from src.nncf import CONMF
 
 
 # Return the block matrix and the beginning of each submatrix
 # GG: block-wise structure matrix 
 # c: block-wise edge weight matrix
+
 def aggregation(As: list, Cs: dict, GG:np.ndarray, size_arr:list, gamma:np.ndarray):
     layer = len(As)
     pos = np.cumsum(size_arr)
@@ -23,21 +25,6 @@ def aggregation(As: list, Cs: dict, GG:np.ndarray, size_arr:list, gamma:np.ndarr
                     blockmat[x, y] = gamma[i,j] * Cs[(i, j)][a, b]
                     blockmat[y, x] = gamma[j,i] * Cs[(i, j)][a, b]
     return blockmat, pos
-
-# 先不考虑Boosting,Boosting并不是一个本质的东西；
-def CorduenWithoutMF(As: list, Cs: dict, GG, c=10):
-    layer = len(As)
-    candidate = [list(range(a.shape[0])) for a in As]
-    size_arr = [0] + [len(layer_cand) for layer_cand in candidate]
-    totalMat,pos = aggregation(As, Cs, GG, size_arr, c)
-    res,score = greedyCharikar(totalMat) # peeling the total multi-layered network
-    res = np.array(sorted(list(res)))
-    finalres = []   # node subset for each layer
-    for i in range(layer):
-        tmp_res = res[(res>=pos[i]) & (res<[pos[i+1]])] - pos[i]
-        finalres.append(tmp_res)
-    return finalres,res,score
-
 
 # Convert the multi-layered network matrix to lil_matrix
 def convertLil(As: list, Cs: dict, GG:np.ndarray):
@@ -78,13 +65,19 @@ def mergeML(As:list,Cs:dict,GG:np.ndarray,gamma:np.ndarray):
     totalMat,_ = aggregation(As, Cs, GG, size_arr, gamma)
     return totalMat
 
+def indicator(res:list):
+    for i in range(len(res)):
+        if len(res[i]) == 0:
+            return 0
+    return 1
+
 
 """ Parameters in corduen
     As : list of within-layer adjacency matrix 
     Cs : dict of cross-layer dependency matrix
     GG : the structure of the dependency in multi-layered network
     gamma: edge importance for cross-layer network
-    boost: boosting or not. (Note: we do not recommend boosting for network with huge nnz.)
+    boost: Optional, boosting or not. (Note: we do not recommend boosting for network with huge NNZ.)
     Ortho: with orthogonal constraints or not
     beta : coupled strength in matrix factorization
     R    : low-rank
@@ -92,18 +85,16 @@ def mergeML(As:list,Cs:dict,GG:np.ndarray,gamma:np.ndarray):
     reg  : l1 regularization
     seed : random seed
 """
-def Corduen(As: list, Cs: dict, GG: np.ndarray, gamma:np.ndarray, boost=True, Ortho=True, beta=None, R=10, epoch=50, reg=1e-10, seed=1234):
-    
 
-    # CONMF or CNMF to get the factor matrix for each layer
+def Corduen(As: list, Cs: dict, GG: np.ndarray, gamma:np.ndarray, boost=True, beta=None, R=10, epoch=50, reg=1e-10, seed=1234):
+
     convertCsc(As,Cs,GG)
-    if Ortho:
-        U, lamb, sigm, beta = CONMF(As, Cs, GG, beta,R,epoch,reg,seed)
-    else:
-        U, lamb, sigm, beta = CNMF(As, Cs, GG, beta,R,epoch,reg,seed)
+    U, lamb, sigm, beta = CONMF(As, Cs, GG, beta,R,epoch,reg,seed)
     convertLil(As,Cs,GG)
 
     layer = len(As)
+    nsize = sum([factor.shape[0] for factor in U])
+
     bestRes   = None
     bestScore = 0
     globalRes = []
@@ -114,69 +105,66 @@ def Corduen(As: list, Cs: dict, GG: np.ndarray, gamma:np.ndarray, boost=True, Or
         # Merge the multi-layered network into a large network
         totalMat, totalPos = aggregation(As, Cs, GG, total_size_arr, gamma)
 
-    nsize = sum([factor.shape[0] for factor in U])
+    
 
     for r in range(R):
-        candidate = []
+        candidate = [None] * layer
         for i in range(layer):
-            # set the truncated threshold as sqrt(1/n) and the top-percent as 1%
-            delta = np.sqrt(1/U[i].shape[0])
-            # topk = int(0.01*U[i].shape[0])
-            topk  = int(0.02*U[i].shape[0]*(1-U[i].shape[0]/nsize)) # a small trick 
-            uThreshold = list(np.argwhere(U[i][:, r] > delta)[:, 0])
-            uPercent = list(np.argpartition(U[i][:,r],-1*topk)[-1*topk:])
-            # Select the node subset with the larger size.
-            if len(uThreshold)<len(uPercent):
-                candidate.append(uPercent)
-            else:
-                candidate.append(uThreshold) 
+
+            # Select the candidate nodes for the i-th layer
+            delta = np.sqrt(1/U[i].shape[0])                        # select the nodes with score larger than delta
+            topk  = int(0.02*U[i].shape[0]*(1-U[i].shape[0]/nsize)) # select the top 1% nodes based on the score
+            cand_layer_delta = sorted(np.argwhere(U[i][:, r] > delta)[:, 0])
+            cand_layer_topk  = sorted(np.argpartition(U[i][:,r],-1*topk)[-1*topk:])
+            candidate[i] = cand_layer_delta if len(cand_layer_delta)>=len(cand_layer_topk) else cand_layer_topk
 
         # Construct the multi-layered subgraph based on the candidate nodes
         tmpAs,tmpCs = sampleML(As,Cs,GG,candidate)
-
         # Aggregation the multi-layered subgraph into a block matrix 
         size_arr = [0] + [len(cand) for cand in candidate]
         blockMat, pos = aggregation(tmpAs, tmpCs, GG, size_arr, gamma)  
-        score = blockMat.sum() / (2*len(candidate))
-        print(f"After candidating, size: {[len(cand) for cand in candidate]}, averaged degree density: {score}")
+        density = blockMat.sum() / (2 * blockMat.shape[0])
+        print("After candidating, size of each layer: ", [len(cand) for cand in candidate])
+        print("The joint density is {:.4f}".format(density))
 
-        # The greedy peeling can be replaced by other DSD solvers.
-        res, score = greedyCharikar(blockMat)  
+        # The greedy_charikar can be replaced by other DSD solvers.
+        res,density = greedyCharikar(blockMat)  
         res = np.array(sorted(list(res)))
 
-        # Split the result into corresponding nodes in each layer
+        # Split the result into the corresponding nodes in each layer
         layerRes = []
         for i in range(layer):
             tmp_res = res[(res >= pos[i]) & (res < [pos[i+1]])] - pos[i]
             tmp_layer_res = [candidate[i][idx] for idx in list(tmp_res)]
             layerRes.append(tmp_layer_res)
 
-        print(f"After greedy peeling, size: {[len(re) for re in layerRes]}, averaged degree density: {score}")
+        print("After dsd detector, size of each layer: ", [len(re) for re in layerRes])
+        print("The joint density is {:.4f}".format(density))
 
         # Boosting for each column vector group.
         if boost:
             totalRes = []
             for i in range(layer):
                 totalRes.extend([idx+totalPos[i] for idx in layerRes[i]])
-            totalRes, score = fastNeighborBoosting(totalMat, totalRes, score)
+            totalRes, density = fastNeighborBoosting(totalMat, totalRes, density)
             totalRes = np.array(sorted(list(totalRes)))
             # Split the totalRes into each layer
             for i in range(layer):
                 layerRes[i] = list(totalRes[(totalRes >= totalPos[i]) & (
                     totalRes < [totalPos[i+1]])] - totalPos[i])
             
-            print(f"After neighbor boosting, size: {[len(re) for re in layerRes]}, averaged degree density: {score}")
+            print("After neighbor boosting, size of each layer: ", [len(re) for re in layerRes])
+            print("The joint density is {:.4f}".format(density))
 
         
-        for i in range(layer):
-            score = 0 if len(layerRes[i])==0 else score
+        density *= indicator(layerRes)    
 
-        print(f"Correlated density of the {r}-th group of column vectors: {score}")
+        print("The optimal joint density of the {}-th column of factors: {:.4f}".format(r,density))
         print('\n')
 
-        if score > bestScore:
-            bestScore = score
-            bestRes = layerRes.copy()
-        globalRes.append((layerRes,score))
+        if density > bestScore:
+            bestScore = density
+            bestRes = layerRes
+        globalRes.append((layerRes,density))
 
     return bestRes,bestScore,globalRes
