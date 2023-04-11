7a80ef5cfcd79166fa4e29ff2f96a16f3fd531a2
Handsomewl <wl.tsinghua.ee@gmail.com>
2023-04-12 03:38:46 +0800

    modify evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,95 +1,51 @@
 import numpy as np
 
-def eva1(As:list,Cs:dict,GG:np.ndarray,nodes:list):
+def evaluation(As:list,Cs:dict,GG:np.ndarray,nodes:list):
     layer = len(As)
     count = 0 
-    mindensity = np.Inf
-    geodensity = 1
-    avgdensity = 0
+    min_density = np.Inf
+    geo_density = 1
+    avg_density = 0
     within = 0
     cross = 0
 
     print("Size of each layer: ", [len(node) for node in nodes])
+    print("Edge density of each network: ")
     
     for i in range(layer):
-        density = 0
+        edge_density = 0
         if len(nodes[i])>=2:
-            density = As[i][nodes[i],:][:,nodes[i]].sum() / (len(nodes[i])*(len(nodes[i])-1))
-        mindensity = min(mindensity,density)
-        geodensity *= density
-        avgdensity += density
-        within += density
+            edge_density = As[i][nodes[i],:][:,nodes[i]].sum() / (len(nodes[i])*(len(nodes[i])-1))
+        min_density = min(min_density,edge_density)
+        geo_density *= edge_density
+        avg_density += edge_density
+        within += edge_density
         count += 1
 
-        print(f"Within-layer {i+1}, the edge density is {density}")
+        print("Within-layer {}, the edge density is {:.4g}".format(i+1,edge_density))
 
         for j in range(layer):
             if j>i and GG[i,j]!=0:
-                density = 0
+                edge_density = 0
                 if len(nodes[i])>=2 and len(nodes[j])>=2:
-                    density = Cs[(i,j)][nodes[i],:][:,nodes[j]].sum() / (len(nodes[i])*len(nodes[j]))
-                mindensity = min(mindensity,density)
-                geodensity *= density
-                avgdensity += density
-                cross += density
+                    edge_density = Cs[(i,j)][nodes[i],:][:,nodes[j]].sum() / (len(nodes[i])*len(nodes[j]))
+                min_density = min(min_density,edge_density)
+                geo_density *= edge_density
+                avg_density += edge_density
+                cross += edge_density
                 count += 1
-                print(f"Cross-layer {i+1,j+1}, the edge density is {density}")
+                print("Cross-layer {}, the edge density is {:.4g}".format((i+1,j+1),edge_density))
 
-    geodensity = np.power(geodensity,1/count)
-    avgdensity /= count
-    crossdensity = within*cross
-    
+    geo_density = np.power(geo_density,1/count)
+    avg_density /= count
+    cross_density = within*cross
 
-    print(f"Minimum edge density {mindensity}")
-    print(f"Geometric mean edge density {geodensity}")
-    print(f"Average mean edge density {avgdensity}")
-    print(f"Cross edge density {crossdensity}")
-
-
-    return mindensity,geodensity,avgdensity,crossdensity
+    print("\n")
+    print("Our defined metric to measure the quality of the mined result:")
+    print("Minimum edge density is {:.4g}".format(min_density))
+    print("Geometric mean edge density is {:.4g}".format(geo_density))
+    print("Average mean edge density is {:.4g}".format(avg_density))
+    print("Cross edge density is {:.4g}".format(cross_density))
 
 
-def eva2(As: list, Cs: dict, GG: np.ndarray, nodes: list):
-    layer = len(As)
-    count = 0
-    mindensity = np.Inf
-    geodensity = 1
-    avgdensity = 0
-    within = 0
-    cross = 0
-
-    for i in range(layer):
-        density = 0
-        if len(nodes[i]) >= 2:
-            density = As[i][nodes[i], :][:, nodes[i]].nnz / (len(nodes[i])*2)
-        mindensity = min(mindensity, density)
-        geodensity *= density
-        avgdensity += density
-        within += density
-        count += 1
-
-        print(f"Within-layer {i+1}, the average density is {density}")
-
-        for j in range(layer):
-            if j > i and GG[i, j] != 0:
-                density = 0
-                if len(nodes[i]) >= 2 and len(nodes[j]) >= 2:
-                    density = Cs[(i, j)][nodes[i], :][:, nodes[j]
-                                                      ].nnz / np.sqrt((len(nodes[i])*len(nodes[j])))
-                mindensity = min(mindensity, density)
-                geodensity *= density
-                avgdensity += density
-                cross += density
-                count += 1
-                print(f"Cross-layer {i+1,j+1}, the average density is {density}")
-                
-    geodensity = np.power(geodensity,1/count)
-    avgdensity /= count
-    crossdensity = within*cross
-
-    print(f"Minimum average density {mindensity}")
-    print(f"Geometric mean average density {geodensity}")
-    print(f"Average mean average density {avgdensity}")
-    print(f"Cross average density {crossdensity}")
-
-    return mindensity, geodensity,avgdensity,crossdensity
+    return min_density,geo_density,avg_density,cross_density
