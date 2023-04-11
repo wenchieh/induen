8e2000b242078be43e7585a4dfc47bbe86b6b4a6
Handsomewl <wl.tsinghua.ee@gmail.com>
2023-04-12 03:26:48 +0800

    add contracter

--- src/boosting.py
+++ src/boosting.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.sparse import lil_matrix
-from src.MinTree import MinTree
 from src.MaxTree import MaxTree
 
 # Give a graph and its indicator vector,
@@ -73,25 +72,24 @@ def neighborBoostingRecursive(graph: lil_matrix, sub: list):
         return subgraph, curAveScore
 
 
-def fastNeighborBoosting(graph: lil_matrix, sub: list, score):
+def fastExpander(graph: lil_matrix, sub: list, score):
 
     # graph: undirected graph adjacency lil_matrix
-    # sub: subgraph that to be extend , array_like data
-    # curScore : edge weights / node numbers
+    # sub: subgraph that to be expand , array_like data
+    # score : sum of edge weights / number of nodes
 
     subgraph = sub.copy()
     curSet = set(subgraph)
     curAveScore = score
     curScore = score * len(subgraph)
     residualNodes = set(range(graph.shape[0])) - curSet
-    residualList = sorted(list(residualNodes))
-    residualPrior = np.squeeze(
-        graph[residualList, :][:, subgraph].sum(axis=1).A)
+    residualList = sorted(residualNodes)
+    residualPrior = np.squeeze(graph[residualList, :][:, subgraph].sum(axis=1).A)
     # Construct the maximum priority tree
     priority = MaxTree(residualPrior)
-    isBoost = True
+    isExpand = True
 
-    while isBoost:
+    while isExpand:
 
         node, prior = priority.getMax()
         if (curScore+prior)/(len(curSet)+1) >= curAveScore:
@@ -108,6 +106,44 @@ def fastNeighborBoosting(graph: lil_matrix, sub: list, score):
                     tmpNeigh = residualList.index(neigh)
                     priority.changeVal(tmpNeigh, delt)
         else:
-            isBoost = False
-    subgraph = sorted(list(curSet))
+            isExpand = False
+    subgraph = sorted(curSet)
+    return subgraph, curAveScore
+
+
+
+def fastContracter(graph: lil_matrix, sub: list, score):
+
+    # graph: undirected graph adjacency lil_matrix
+    # sub: subgraph that to be contract , array_like data
+    # score : sum of edge weights / number of nodes
+
+    subgraph = sub.copy()
+    curSet = set(subgraph)
+    curAveScore = score
+    curScore = score * len(subgraph)
+    curPrior = np.squeeze(graph[subgraph, :][:, subgraph].sum(axis=1).A)
+    # Construct the maximum priority tree
+    priority = MinTree(curPrior)
+    isContract = True
+
+    while isContract:
+
+        node, prior = priority.getMin()
+        if (curScore-prior)/(len(curSet)-1) >= curAveScore:
+            curScore -= prior
+            trueNode = subgraph[node]
+            curSet.remove(trueNode)
+            curAveScore = curScore / len(curSet)
+
+            # Change the priority of the node and its neighbors
+            priority.changeVal(node, float('inf'))
+            for neigh in graph.rows[trueNode]:
+                if neigh in curSet:
+                    delt = graph[trueNode, neigh]
+                    tmpNeigh = subgraph.index(neigh)
+                    priority.changeVal(tmpNeigh, delt)
+        else:
+            isContract = False
+    subgraph = sorted(curSet)
     return subgraph, curAveScore
