fa393fcc4abe48265503d074c2f06bb1dd823cb0
Handsomewl <wl.tsinghua.ee@gmail.com>
2023-04-12 16:40:32 +0800

    simplify

--- src/boosting.py
+++ src/boosting.py
@@ -3,75 +3,9 @@ from scipy.sparse import lil_matrix
 from src.MaxTree import MaxTree
 from src.MinTree import MinTree
 
-# Give a graph and its indicator vector,
-# Extend the subgraph with higher score with the neighbor nodes.
-
-
-def neighborBoosting(graph: lil_matrix, sub: list):
-
-    # graph: undirected graph adjacency lil_matrix
-    # sub: subgraph that to be extend , array_like data
-    # curScore : edge weights / node numbers
-
-    subgraph = sub.copy()
-    curSet = set(subgraph)
-    addSet = set()
-    curScore = graph[subgraph, :][:, subgraph].sum() / 2
-    curAveScore = curScore / len(curSet)
-
-    # Initialize addSet as all the neighbors of the current subgraph
-    for node in subgraph:
-        subSet = set(graph.rows[node]) - curSet
-        addSet = addSet | subSet
-
-    # Loop until there are no node in addSet can boosting the score
-    for neigh in addSet:
-        addScore = graph[neigh][:, subgraph].sum()
-
-        if (curScore+addScore) / (len(curSet)+1) >= curAveScore:
-            curScore += addScore
-            curSet.add(neigh)
-            curAveScore = curScore / len(curSet)
-
-            subgraph.append(neigh)
-            subSet = set(graph.rows[neigh]) - curSet
-            addSet = addSet | subSet
-
-    return subgraph, curAveScore
-
-
-def neighborBoostingRecursive(graph: lil_matrix, sub: list):
-    # graph: undirected graph adjacency lil_matrix
-    # sub: subgraph that to be extend , array_like data
-    # curScore : edge weights / node numbers
-
-    subgraph = sub.copy()
-    curSet = set(subgraph)
-    addSet = set()
-    curScore = graph[subgraph, :][:, subgraph].sum() / 2  # edges
-    curAveScore = curScore / len(curSet)
-
-    # Initialize addSet as all the neighbors of the current subgraph
-    for node in subgraph:
-        subSet = set(graph.rows[node]) - curSet
-        addSet = addSet | subSet
-
-    addList = list(addSet)
-    addScores = [graph[neigh][:, subgraph].sum() for neigh in addList]
-    pos = np.argmax(addScores)
-    max_prior_node = addList[pos]
-    addScore = addScores[pos]
-
-    # Loop until the node with maximum priority can boost the score
-    if (curScore+addScore) / (len(curSet)+1) >= curAveScore:
-        curScore += addScore
-        curSet.add(max_prior_node)
-        curAveScore = curScore / curSet
-        subgraph.append(max_prior_node)
-        return neighborBoostingRecursive(graph, subgraph)
-    else:
-        return subgraph, curAveScore
-
+""" Give a graph, its aubgraph i.e. an indicator vector, score of the aveage degree
+    Expand and contract the subgraph with higher score with the neighbor nodes.
+"""
 
 def fastExpander(graph: lil_matrix, sub: list, score):
 
@@ -110,7 +44,6 @@ def fastExpander(graph: lil_matrix, sub: list, score):
             isExpand = False
     subgraph = sorted(curSet)
     return subgraph, curAveScore
-
 
 
 def fastContracter(graph: lil_matrix, sub: list, score):
